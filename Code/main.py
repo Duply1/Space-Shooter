e5bab8c8ffe3779803c39565944fb8f8ec7e462d
Duply1 <aniket.bhattacharjee@gmail.com>
2025-02-17 18:35:27 -0500

    Starting of the Pygame Projects

---
+++ Code/main.py
@@ -0,0 +1,44 @@
+import pygame 
+from os.path import join #importing so that this code can dynamic
+from random import randint
+
+
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+
+#display surface
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space Shooter')
+running = True
+#Window = pygame.Window(title="Space Shooter", position=(WINDOW_WIDTH, WINDOW_HEIGHT))
+
+#game surface 
+surf = pygame.Surface((100,200)) 
+surf.fill('orange')
+x=100 
+
+
+#if your image has transparent pixels, use conver_alpha or else use convert 
+#this helps the pygame to generate frames faster 
+player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
+star_position = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
+while running:
+    #event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    
+    display_surface.fill('darkgrey')
+    #blit = Block Image Transfer
+    x += 0.1
+    display_surface.blit(player_surf, (x, 150))
+    for pos in star_position:
+        display_surface.blit(star_surf, pos)
+    pygame.display.update()
+    
+
+
+pygame.quit()
