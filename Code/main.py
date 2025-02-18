d945a237389e8c58ea327959e0d58a72c3bab573
Duply1 <aniket.bhattacharjee@gmail.com>
2025-02-17 19:05:31 -0500

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -21,8 +21,15 @@ x=100
 #if your image has transparent pixels, use conver_alpha or else use convert 
 #this helps the pygame to generate frames faster 
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+
+
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_position = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
+
+laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (WINDOW_WIDTH, WINDOW_HEIGHT))
 
 while running:
     #event loop
@@ -33,10 +40,21 @@ while running:
     
     display_surface.fill('darkgrey')
     #blit = Block Image Transfer
-    x += 0.1
-    display_surface.blit(player_surf, (x, 150))
+    #Making Animations
+    #x += 0.1
+
     for pos in star_position:
         display_surface.blit(star_surf, pos)
+
+    #Making Bouncing Animations
+    #if player_rect.right < WINDOW_WIDTH:
+       # player_rect.left += 0.1
+   # elif player_rect.left == WINDOW_WIDTH:
+    #    player_rect.right -= 0.1
+
+    display_surface.blit(player_surf, player_rect)
+    display_surface.blit(laser_surf, laser_rect)
+    
     pygame.display.update()
     
 
